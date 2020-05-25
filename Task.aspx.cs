c85fcfc1c2d390660ddb51064147c7b3350b5aa4
danil355 <52044554+danil355@users.noreply.github.com>
2020-05-25 20:39:00 +0600

    Add files via upload

---
+++ Task.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_LESSON_3
+{
+    public partial class Task : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*Request.Cookies.Get("");
+
+            Response.Cookies.Set(...);*/
+
+            /*Session_ID
+            Session["data"] = 10;*/
+
+
+            /*ViewState[]*/
+        }
+    }
+}
